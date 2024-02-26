7b222ece10bf21cc80e500983233dcf67561fb77
Crevan <a.siverskii@gmail.com>
2024-02-26 09:49:17 +0300

    initial

---
+++ src/main/java/dev/crevan/jwt/JwtDemo.java
@@ -0,0 +1,11 @@
+package dev.crevan.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(JwtDemo.class, args);
+    }
+}
